435173e17d9e0a7ee4bc68bc7cbfbd83ccc36306
Emanuele 'Adekro' Croce <e.croce88@gmail.com>
2023-07-25 16:18:51 +0200

    add getprojectmenuitem

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,6 +2,6 @@ import "./components/style/general.css";
 import Login from "./components/Login/Login";
 import useLogin from "./hooks/useLogin";
 import ProjectMenuForm from "./components/ProjectMenuForm/ProjectMenuForm";
-import getProgectMenuItem from "./components/ProjectMenuForm/ProjectMenuForm";
+import { getProgectMenuItem } from "./components/ProjectMenuForm/ProjectMenuForm";
 
 export { Login, useLogin, ProjectMenuForm, getProgectMenuItem };
