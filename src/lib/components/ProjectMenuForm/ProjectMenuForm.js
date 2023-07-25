b8bb4e9f6c05ff7669d87343197ab78ab1629191
Emanuele 'Adekro' Croce <e.croce88@gmail.com>
2023-07-25 16:09:38 +0200

    add getprojectmenuitem

--- src/lib/components/ProjectMenuForm/ProjectMenuForm.js
+++ src/lib/components/ProjectMenuForm/ProjectMenuForm.js
@@ -1,10 +1,40 @@
 import React from "react";
+import { Button } from "axonaui";
 const ProjectMenuForm = ({ idItem }) => {
   return (
     <div>
-      <p>Anni</p>
-      <input type="date" />
+      {idItem === "anni" && (
+        <>
+          <p>Anni</p>
+          <input type="date" />
+        </>
+      )}
+      {idItem === "conta" && (
+        <>
+          <p>Contabilizzazione</p>
+          <Button>Selezionata</Button>
+        </>
+      )}
     </div>
   );
 };
 export default ProjectMenuForm;
+
+export const getProgectMenuItem = () => {
+  const itemspj = [
+    {
+      id: "anni",
+      label: "Anni",
+      img: "calendar",
+      function: () => {},
+    },
+    {
+      id: "conta",
+      label: "Contabilizzazione",
+      img: "print",
+      function: () => {},
+    },
+  ];
+
+  return itemspj;
+};
