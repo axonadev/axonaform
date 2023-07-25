fb17afc954e9ba28ba949701613a21f1ef3355e6
Emanuele 'Adekro' Croce <e.croce88@gmail.com>
2023-07-25 15:30:44 +0200

    app projectmenuform

---
+++ src/lib/components/ProjectMenuForm/ProjectMenuForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+const ProjectMenuForm = ({ idItem }) => {
+  return (
+    <div>
+      <p>Anni</p>
+      <input type="date" />
+    </div>
+  );
+};
+export default ProjectMenuForm;
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,6 @@
 import "./components/style/general.css";
 import Login from "./components/Login/Login";
 import useLogin from "./hooks/useLogin";
+import ProjectMenuForm from "./components/ProjectMenuForm/ProjectMenuForm";
 
-export { Login, useLogin };
+export { Login, useLogin, ProjectMenuForm };
